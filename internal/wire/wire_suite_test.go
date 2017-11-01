8d4a5fa724e45fcc2b9e0a596c93769021e725c8
Marten Seemann <martenseemann@gmail.com>
2017-11-01 15:29:15 +0700

    implement the MAX_DATA and MAX_STREAM_DATA frames
    For gQUIC WINDOW_UPDATEs are converted to MAX_DATA and MAX_STREAM_DATA,
    respectively.

--- internal/wire/wire_suite_test.go
+++ internal/wire/wire_suite_test.go
@@ -15,11 +15,20 @@ func TestCrypto(t *testing.T) {
 }
 
 const (
-	versionLittleEndian = protocol.Version37 // a QUIC version that uses little endian encoding
-	versionBigEndian    = protocol.Version39 // a QUIC version that uses big endian encoding
+	// a QUIC version that uses little endian encoding
+	versionLittleEndian = protocol.Version37
+	// a QUIC version that uses big endian encoding
+	versionBigEndian = protocol.Version39
+	// a QUIC version that uses stream 0 as the crypto stream.
+	// That means it also has to use the MAX_DATA / MAX_STREAM_DATA and BLOCKED / STREAM_BLOCKED frames
+	versionCryptoStream0 = protocol.VersionTLS
 )
 
 var _ = BeforeSuite(func() {
 	Expect(utils.GetByteOrder(versionLittleEndian)).To(Equal(utils.LittleEndian))
 	Expect(utils.GetByteOrder(versionBigEndian)).To(Equal(utils.BigEndian))
+	Expect(utils.GetByteOrder(versionCryptoStream0)).To(Equal(utils.BigEndian))
+	Expect(versionLittleEndian.CryptoStreamID()).To(Equal(protocol.StreamID(1)))
+	Expect(versionBigEndian.CryptoStreamID()).To(Equal(protocol.StreamID(1)))
+	Expect(versionCryptoStream0.CryptoStreamID()).To(Equal(protocol.StreamID(0)))
 })
